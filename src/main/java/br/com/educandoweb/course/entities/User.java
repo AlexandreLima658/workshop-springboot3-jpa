4aa56e3cf0b823c14894659b5874fb7ca6cccb4c
Carlos Alexandre <alexandrec@alu.ufc.br>
2024-03-12 10:37:10 -0300

    feat: user entity and resource

--- src/main/java/br/com/educandoweb/course/entities/User.java
+++ src/main/java/br/com/educandoweb/course/entities/User.java
@@ -2,31 +2,32 @@ package br.com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.util.Objects;
-import java.util.UUID;
 
 public class User implements Serializable {
 
   private static final long serialVersionUID = 1L;
-  private UUID id;
+  private Long id;
   private String name;
+  private String email;
   private String phone;
   private String password;
-  private String number;
+
 
   public User() {}
 
-  public User(UUID id, String name, String phone, String password) {
+  public User(Long id, String name, String email, String phone, String password) {
     this.id = id;
     this.name = name;
+    this.email = email;
     this.phone = phone;
     this.password = password;
   }
 
-  public UUID getId() {
+  public Long getId() {
     return id;
   }
 
-  public void setId(UUID id) {
+  public void setId(Long id) {
     this.id = id;
   }
 
@@ -36,6 +37,14 @@ public class User implements Serializable {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public String getEmail(){
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
   }
 
   public String getPhone() {
